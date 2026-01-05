16210752ed962a412d4f014459840b3a2fbcd162
Carlos Henrique <ch.henriquevinha@gmail.com>
2026-01-04 23:12:10 -0300

    **clean**

--- concepts/testing/jest/demo/src/sum.ts
+++
@@ -1,4 +0,0 @@
-export const sum = (a: number, b: number) => a + b;
-export const sub = (a: number, b: number) => a - b;
-export const mult = (a: number, b: number) => a * b;
-export const div = (a: number, b: number) => a / b;
